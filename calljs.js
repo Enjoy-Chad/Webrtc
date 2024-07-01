e073b02f861c8bcfac4d9df683f2d32f691c48e9
Chad El Kurdi <73014385+Chadek24295@users.noreply.github.com>
2024-07-01 17:54:54 +0200

    rooms

--- calljs.js
+++ calljs.js
@@ -1,101 +1,121 @@
+var peer = null;
+var myStream = null;
+var peerList = [];
 
-var peer = new Peer()
-var myStream
-var peerList = []
+// Initialize PeerJS with a unique ID (userId)
+function init(userId) {
+    peer = new Peer(userId);
 
+    peer.on('open', (id) => {
+        console.log(id + " connected");
+    });
 
-//this function will be initiating the peer
-function init(userId){
-  peer = new Peer(userId)
-  peer.on('open',(id)=>{
-    console.log(id+" connected") //if we connect successfully this will print
-  })
-
-  listenToCall()
+    listenToCall();
 }
 
+// Listen for incoming calls
+function listenToCall() {
+    peer.on('call', (call) => {
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then((stream) => {
+            myStream = stream;
+            addLocalVideo(stream);
+            call.answer(stream);
+            handleCall(call);
+        }).catch((err) => {
+            console.error("Unable to get user media:", err);
+        });
+    });
+}
 
-//this function will keep listening to call or incoming events
-function listenToCall(){
-  peer.on('call',(call)=>{
-    navigator.mediaDevices.getUserMedia({
-      video:true,
-      audio: true
-    }).then((stream)=>{
-
-      myStream = stream
-      addLocalVideo(stream)
-      call.answer(stream)
-      call.on('stream',(remoteStream)=>{
-        if(!peerList.includes(call.peer)){
-          addRemoteVideo(remoteStream)
-          peerList.push(call.peer)
+// Function to handle a call object
+function handleCall(call) {
+    call.on('stream', (remoteStream) => {
+        if (!peerList.includes(call.peer)) {
+            addRemoteVideo(remoteStream);
+            peerList.push(call.peer);
         }
-      })
-    }).catch((err)=>{
-      console.log("unable to connect because "+err)
-    })
-  })
+    });
+
+    // Store the call object in peerList
+    peerList.push(call.peer);
 }
 
-//this function will be called when we try to make a call
-function makeCall(receiverId){
+// Function to join a room (make a call to all current peers in the room)
+function joinRoom(roomId) {
     navigator.mediaDevices.getUserMedia({
-      video:true,
-      audio: true
-    }).then((stream)=>{
-      myStream = stream
-      addLocalVideo(stream)
-      let call = peer.call(receiverId,stream)
-      call.on('stream',(remoteStream)=>{
-        if(!peerList.includes(call.peer)){
-          addRemoteVideo(remoteStream)
-          peerList.push(call.peer)
+        video: true,
+        audio: true
+    }).then((stream) => {
+        myStream = stream;
+        addLocalVideo(stream);
+
+        // Call all existing peers in the room
+        for (let peerId of peerList) {
+            let call = peer.call(peerId, stream);
+            handleCall(call);
         }
-      })
-    }).catch((err)=>{
-      console.log("unable to connect because "+err)
-    })
 
+        // Listen for new incoming calls
+        peer.on('call', (call) => {
+            call.answer(stream);
+            handleCall(call);
+        });
+
+        // Add current peer to its own peerList
+        peerList.push(peer.id);
+    }).catch((err) => {
+        console.error("Unable to get user media:", err);
+    });
 }
 
-//this function will add local stream to video pannel
-function addLocalVideo(stream){
-  let video = document.createElement("video")
-  video.srcObject = stream
-  video.classList.add("video")
-  video.muted = true // local video need to be mute because of noise issue
-  video.play()
-  document.getElementById("localVideo").append(video)
+// Function to make a call to a specific receiverId (join a room or start a new call)
+function makeCall(receiverId) {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then((stream) => {
+        myStream = stream;
+        addLocalVideo(stream);
+
+        let call = peer.call(receiverId, stream);
+        handleCall(call);
+    }).catch((err) => {
+        console.error("Unable to get user media:", err);
+    });
 }
 
-//this function will add remote stream to video pannel
-function addRemoteVideo(stream){
-  let video = document.createElement("video")
-  video.srcObject = stream
-  video.classList.add("video")
-  video.play()
-  document.getElementById("remoteVideo").append(video)
+// Function to add local stream to video panel
+function addLocalVideo(stream) {
+    let video = document.createElement("video");
+    video.srcObject = stream;
+    video.classList.add("video");
+    video.muted = true; // Local video needs to be muted
+    video.play();
+    document.getElementById("localVideo").append(video);
 }
 
-
-
-//so far video call is working fine.. lets toggle the video or audio
-function toggleVideo(b){
-  if(b=="true"){
-    myStream.getVideoTracks()[0].enabled = true
-  }
-  else{
-    myStream.getVideoTracks()[0].enabled = false
-  }
+// Function to add remote stream to video panel
+function addRemoteVideo(stream) {
+    let video = document.createElement("video");
+    video.srcObject = stream;
+    video.classList.add("video");
+    video.play();
+    document.getElementById("remoteVideo").append(video);
 }
 
-//toggle audio
-function toggleAudio(b){
-  if(b=="true"){
-    myStream.getAudioTracks()[0].enabled = true
-  }
-  else{
-    myStream.getAudioTracks()[0].enabled = false
-  }
+// Toggle video track on/off
+function toggleVideo(enabled) {
+    if (myStream) {
+        myStream.getVideoTracks()[0].enabled = enabled;
+    }
+}
+
+// Toggle audio track on/off
+function toggleAudio(enabled) {
+    if (myStream) {
+        myStream.getAudioTracks()[0].enabled = enabled;
+    }
 }
